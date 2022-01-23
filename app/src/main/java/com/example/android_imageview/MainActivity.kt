d51336794e3d6eda5268305753130519e861f020
Abdulaizizphp <95176633+Abdulaizizphp@users.noreply.github.com>
2022-01-23 21:51:37 +0300

    Initial commit

---
+++ app/src/main/java/com/example/android_imageview/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.android_imageview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val imageView = findViewById<ImageView>(R.id.logo2)
+        imageView.setImageR
+    }
+}
